89d6559fc4e60cd7776716e4a4a8b0ed04528fa3
erique <eriqual@gmail.com>
2016-06-09 10:51:16 +0200

    partialOrganism class

---
+++ src/taBOO/PartialOrganism.java
@@ -0,0 +1,47 @@
+package taBOO;
+
+public class PartialOrganism {
+	
+	/**
+	 * identifier
+	 */
+	private String gi;
+	
+	/**
+	 * Sequence of
+	 */
+	private String seq;
+	
+	/**
+	 * 
+	 */
+	private String revSeq;
+	
+	/**
+	 * 
+	 * @param fastaChunk
+	 */
+	public PartialOrganism(String fastaChunk) {
+		fastaSplitter(fastaChunk);
+	}
+
+	/**
+	 * 
+	 * @param fastaChunk
+	 */
+	private void fastaSplitter(String fastaChunk) {
+		String[] headAndShoulders= fastaChunk.split("\n");
+		gi = headAndShoulders[0];
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1; i < headAndShoulders.length; i++) {
+			sb.append(headAndShoulders[i]);
+		}
+		seq = sb.toString();
+	}
+	
+	public static void main() {
+		
+	}
+	
+}
