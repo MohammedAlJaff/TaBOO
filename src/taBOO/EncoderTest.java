41e5e66e05aacdd07c11ea92826baf8201949bbc
erique <eriqual@gmail.com>
2016-06-14 14:52:55 +0200

    Fixed Encoder and EncoderTest

--- src/taBOO/EncoderTest.java
+++ src/taBOO/EncoderTest.java
@@ -3,6 +3,9 @@ package taBOO;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import taBOO.Encoder.EncoderException;
+
 import java.util.*;
 
 public class EncoderTest {
@@ -12,9 +15,9 @@ public class EncoderTest {
 		String s1 = "AATT";
 		String s2 = "AATA";
 
-		boolean b1 = Encoder.kmm(s1, s2, 0);
-		boolean b2 = Encoder.kmm(s1,s2,1);
-		boolean b3 = Encoder.kmm(s1,s2,2);
+		boolean b1 = Encoder.withinKMismatches(s1, s2, 0);
+		boolean b2 = Encoder.withinKMismatches(s1,s2,1);
+		boolean b3 = Encoder.withinKMismatches(s1,s2,2);
 		
 		boolean[] actual =  {b1, b2, b3};
 		boolean[] expected = {false, true, true};
@@ -43,8 +46,8 @@ public class EncoderTest {
 	}
 
 	@Test 
-	public void encodeStringToArrayTest1() {
-		Encoder e = new Encoder();
+	public void encodeStringToArrayTest1() throws EncoderException {
+		Encoder e = new Encoder(5);
 		
 		String s1 = "AAAAATTTTT";
 		String s2 = "AAAACAAAAG";
@@ -56,9 +59,9 @@ public class EncoderTest {
 				{1023, 1022}
 		};
 		
-		boolean b1 = java.util.Arrays.equals(expRes[0], e.encode5(s1) ) ;
-		boolean b2 = java.util.Arrays.equals(expRes[1], e.encode5(s2) ) ;
-		boolean b3 = java.util.Arrays.equals(expRes[2], e.encode5(s3) ) ;
+		boolean b1 = java.util.Arrays.equals(expRes[0], e.encode(s1) ) ;
+		boolean b2 = java.util.Arrays.equals(expRes[1], e.encode(s2) ) ;
+		boolean b3 = java.util.Arrays.equals(expRes[2], e.encode(s3) ) ;
 		
 		boolean[] actual = {b1,b2,b3};
 		boolean[] expected = {true, true, true};
@@ -66,4 +69,81 @@ public class EncoderTest {
 		assertArrayEquals(expected, actual);
 	}
 
+	@Test
+	public void MismatchesWithNTest1() throws EncoderException {
+		String[] s = {"AANN", "AANA", "AATN", "AAAA"};
+		Encoder e = new Encoder(2);
+		int[] encoded1 = e.encode(s[0]);
+		int[] encoded2 = e.encode(s[1]);
+		int[] encoded3 = e.encode(s[2]);
+		int[] encoded4 = e.encode(s[3]);
+		
+		int[] exp1 = {0, 24};
+		int[] exp2 = {0, 20};
+		int[] exp3 = {0, 19};
+		int[] exp4 = {1, 1};
+		
+		boolean b1 = Arrays.equals(encoded1, exp1);
+		boolean b2 = Arrays.equals(encoded2, exp2);
+		boolean b3 = Arrays.equals(encoded3, exp3);
+		boolean b4 = Arrays.equals(encoded4, exp4);
+		
+		boolean[] actual = {b1, b2, b3, b4};
+		boolean[] expected = {true, true, true, false};
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void withinKMismatchesNcoded() throws EncoderException {
+		String s1 = "AATT";
+		String s2 = "AATA";
+		Encoder e = new Encoder(4);
+		
+		int[] c1 = e.encode(s1);
+		int[] c2 = e.encode(s2);
+
+		boolean b1 = e.withinKMismatches(c1, c2, 0);
+		boolean b2 = e.withinKMismatches(c1, c2, 1);
+		boolean b3 = e.withinKMismatches(c1, c2, 2);
+		
+		boolean[] actual =  {b1, b2, b3};
+		boolean[] expected = {false, true, true};
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void numbMismatchNCodeNumberOfMistachesTest1() throws EncoderException	{
+		String s1 = "AATT";
+		String s2 = "AATA";
+		String s3 = "ATTT";
+		String s4 = "TTTT";
+		
+		Encoder e = new Encoder(4);
+		int[] c1 = e.encode(s1);
+		int[] c2 = e.encode(s2);
+		int[] c3 = e.encode(s3);
+		int[] c4 = e.encode(s4);
+ 
+		int b0 = e.numbMismatch(c1, c1);
+		int b1 = e.numbMismatch(c1, c2);
+		int b2 = e.numbMismatch(c1, c3);
+		int b3 = e.numbMismatch(c2, c3);
+		int b4 = e.numbMismatch(c1, c4);
+		int b5 = e.numbMismatch(c3, c4);
+		
+		int[] actual =  {b0, b1, b2, b3, b4, b5};
+		int[] expected = {0, 1, 1, 2, 2, 1};
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test (expected = EncoderException.class)
+	public void exceptionIllegalLengthTest1() throws EncoderException {
+		Encoder e = new Encoder(4);
+		String s1 = "ATATAT";
+		
+		e.encode(s1);
+	}
 }
